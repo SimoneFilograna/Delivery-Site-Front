1cf9dfe547abc1743f5e2d834a7b6b258bc7cccf
riccardo-giuri <richicryst@gmail.com>
2023-11-02 16:30:38 +0100

    Added and stylized navbar to the homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,24 @@
 <script>
+    import axios from 'axios';
+
     export default {
 
     }
 </script>
 
 <template>
-    <h1>Ciao</h1>
+    <nav >
+
+    </nav>
 </template>
 
 <style lang="scss" scoped>
-
+    .logo{
+    color: white;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 800;
+    text-transform: uppercase;
+    font-size: 1rem;
+    letter-spacing: 4px;
+  }
 </style>
