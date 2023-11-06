6d9416628c37622671cc5b4d4c9a441d81345364
Valerio <valerio.collura@live.it>
2023-11-06 15:53:04 +0100

    added restaurantshowpage route on router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // Importazione pagine
 
+import RestaurantShowPag from "./pages/RestaurantShowPage.vue";
 import HomePage from "./pages/Home.vue";
 
 const routes= [
@@ -9,7 +10,21 @@ const routes= [
         path: "/",
         name: "home",
         component: HomePage
-    }
+    },
+    {
+        path:"/restaurant/:id",
+        name:"restaurant.show",
+        component: RestaurantShowPage,        
+    },
+    
+   /* / / / / / / / / / / / / / / / / / rotta pagina errore da decommentare in caso di implementazione / / / / / / / / / / / / / / / / / 
+    {
+        "path": "/:pathMatch(.*)*",
+        "name": "not-found",
+        "component": NotFoundPage
+      },
+    
+    */
 ]
 
 const router= createRouter ({
