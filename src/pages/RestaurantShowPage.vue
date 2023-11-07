21b4675c24641b04a7633df6fc10734517c1e31f
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-11-07 17:37:05 +0100

    implementes add and remove items in cart

--- src/pages/RestaurantShowPage.vue
+++ src/pages/RestaurantShowPage.vue
@@ -4,74 +4,98 @@ import axios from 'axios';
 import PlateCard from '../components/PlateCard.vue'
 
 export default {
-    components:{
+    components: {
         PlateCard,
     },
-  data() {
-    return {
-      restaurant: {},
-      cartItems: [],
-    };
-  },
-  methods:{
-    //fetching restaurant data from api
-    fetchData(){
-        axios.get('http://127.0.0.1:8000/api/restaurants/'+this.$route.params.id)
-        .then((resp)=>{
-            this.restaurant = resp.data;
-        }) .catch((e)=>{
-            this.$router.push({name: "not-found"})
-        }) 
+    data() {
+        return {
+            restaurant: {},
+            cartItems: [],
+        };
     },
-    //fetching img data from backend
-    getRestaurantImageUrl(restaurant) {
-      return `http://127.0.0.1:8000/storage/${restaurant.restaurant_image}`;
+    methods: {
+        //fetching restaurant data from api
+        fetchData() {
+            axios.get('http://127.0.0.1:8000/api/restaurants/' + this.$route.params.id)
+                .then((resp) => {
+                    this.restaurant = resp.data;
+                }).catch((e) => {
+                    this.$router.push({ name: "not-found" })
+                })
+        },
+        //fetching img data from backend
+        getRestaurantImageUrl(restaurant) {
+            return `http://127.0.0.1:8000/storage/${restaurant.restaurant_image}`;
+        },
+
+        //function that obtain and manipoulate data plate
+
+        addNewItem(element = null) {
+
+            //find index cart item
+            const checkIndexArray = this.cartItems.findIndex(item => item.id === element.id);
+            console.log(checkIndexArray)
+
+
+            if (checkIndexArray !== -1) {
+                // if plate is in cartItems
+                this.cartItems[checkIndexArray].quantity++
+                console.log(this.cartItems)
+            } else {
+                // create new item cart
+                this.cartItems.push({
+                    id: element.id,
+                    plate_name: element.plate_name,
+                    price: element.price,
+                    quantity: 1,
+                    restaurant_id: element.restaurant_id
+                })
+                console.log(this.cartItems)
+            }
+
+            //save in localstorage
+            localStorage.setItem('cartItems', JSON.stringify(this.cartItems))
+        },
+
+        //remove entire cart
+
+        removeItem(index) {
+            this.cartItems.splice(index, 1);
+
+            //save in localstorage
+            localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+        },
+
+        // remove item from crat with icon
+
+        reduceItem(index) {
+            if (this.cartItems[index].quantity > 1) {
+                this.cartItems[index].quantity--; // Reduce quantity
+            } else {
+                // if 1 remove form cart
+                this.removeItem(index);
+            }
+            console.log(this.cartItems)
+            localStorage.setItem('cartItems', JSON.stringify(this.cartItems))
+        },
+
+
     },
+    mounted() {
 
-    //function that receive data from card emit
-    addItemToCart(element){
-        this.cartItems.push(element);
-        localStorage.setItem('cartItems', JSON.stringify(this.cartItems))
+        // Load items from localStorage on component mount 
+        this.cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+
+        //load restaurant API Call
+        this.fetchData();
     },
-
-    // add item to cart with icon
-
-    addNewItem(element){
-        this.cartItems.push(element);
-        console.log(this.cartItems)
-        localStorage.setItem('cartItems', JSON.stringify(this.cartItems))
-    },
-
-    // remove item from crat with icon
-
-    removeItem(index){
-        this.cartItems.splice(index, 1);
-        console.log(this.cartItems)
-        localStorage.setItem('cartItems', JSON.stringify(this.cartItems))
-    },
-
-    // clearCart(){
-    //     this.cartItems = {};
-    //     localStorage.setItem('cartItems', JSON.stringify(this.cartItems))
-    // }
-
-
-  },
-  mounted() {
-
-    // Load items from localStorage on component mount 
-    this.cartItems = JSON.parse(localStorage.getItem('cartItems')) || []; 
-
-    //load restaurant API Call
-    this.fetchData();
-  },
 }
 
 </script>
 
 <template>
     <h1>ciao</h1>
- 
+
 
     <div class="container">
         <div class="row">
@@ -82,14 +106,14 @@ export default {
                 <div class="plates-container">
                     <div class="row">
                         <div class="col-12" v-for="plate in restaurant.plates">
-                            
+
                             <!-- @addcart receive emit from son platecard -->
-                            <PlateCard :Plate="plate" @addtoCart="addItemToCart"/>
+                            <PlateCard :Plate="plate" @addtoCart="addNewItem" />
                         </div>
 
                     </div>
                 </div>
-                
+
             </div>
 
             <!-- Shopping Cart -->
@@ -110,9 +134,11 @@ export default {
                             <i class="fa-solid fa-plus"></i>
                         </button>
 
+                        <!-- quantity-- -->
+                        <p class="px-4">{{ item.quantity }}</p>
                         <!-- remove button cart -->
 
-                        <button @click="removeItem(index)">
+                        <button @click="reduceItem(index)">
                             <i class="fa-solid fa-minus"></i>
                         </button>
                     </div>
@@ -123,9 +149,6 @@ export default {
             </div>
         </div>
     </div>
-
-
-
 </template>
 
 <style lang="scss" scoped>
@@ -134,8 +157,7 @@ export default {
     min-height: 20px;
 }*/
 
-.card-img-top{
+.card-img-top {
     max-width: 30vw;
 }
-
 </style>
