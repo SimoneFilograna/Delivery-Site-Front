c9089af4c85ed354e4bd6e514ac7dda0a73f89f6
DarioEsposito89 <darioesposito.work@gmail.com>
2023-11-08 16:05:43 +0100

    add form delivery validation
    added form for customer data used for shipping

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -6,7 +6,21 @@
             return {
                 clientToken: "",
                 dropinInstance: null,
-                showUI: true
+                showUI: true,
+                // ... Altre variabili esistenti
+                customer_name: "",
+                customer_lastname: "",
+                customer_address: "",
+                customer_email: "",
+                customer_phone: "",
+
+                formErrors: {
+                customer_name: "",
+                customer_lastname: "",
+                customer_address: "",
+                customer_email: "",
+                customer_phone: ""
+                }
             }
         },
 
@@ -30,6 +44,54 @@
                     })
             },
 
+            validateForm() {
+            let isValid = true;
+
+            if (!this.customer_name) {
+                this.formErrors.customer_name = "Il campo Nome è obbligatorio.";
+                isValid = false;
+            } else {
+                this.formErrors.customer_name = "";
+            }
+
+            if (!this.customer_lastname) {
+                this.formErrors.customer_lastname = "Il campo Cognome è obbligatorio.";
+                isValid = false;
+            } else {
+                this.formErrors.customer_lastname = "";
+            }
+
+            if (!this.customer_address) {
+                this.formErrors.customer_address = "Il campo Indirizzo è obbligatorio.";
+                isValid = false;
+            } else {
+                this.formErrors.customer_address = "";
+            }
+
+            if (!this.customer_email) {
+            this.formErrors.customer_email = "Il campo Email è obbligatorio.";
+            isValid = false;
+            } else {
+            // Check if email format is valid
+            const regex = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if (!regex.test(this.customer_email)) {
+            this.formErrors.customer_email = "Email non valida.";
+            isValid = false;
+            } else {
+            this.formErrors.customer_email = "";
+            }
+        }
+
+            if (!this.customer_phone) {
+                this.formErrors.customer_phone = "Il campo Recapito telefonico è obbligatorio.";
+                isValid = false;
+            } else {
+                this.formErrors.customer_phone = "";
+            }
+
+            return isValid;
+        },
+
             AddDropInUI() {
                 braintree.dropin.create({
                 authorization: this.clientToken, 
@@ -44,7 +106,7 @@
             },
 
             submitPayment() {
-                if (this.dropinInstance) {
+            if (this.validateForm() && this.dropinInstance) {
                     const self = this;
 
                     this.dropinInstance.requestPaymentMethod(function (err, payload) {
@@ -72,13 +134,70 @@
 
 <template>
     <div>
-        <div class="container" v-show="showUI">
-                <div id="dropin-container"></div>
-                <button @click="submitPayment()" class="btn btn-primary">Submit Payment</button>
+        <div class="container mt-1" v-show="showUI">
+            <h1>Inserisci coordinate di pagamento</h1>
+            <div id="dropin-container"></div>
+            <button @click="submitPayment()" class="btn btn-primary">Conferma pagamento</button>
         </div>
   </div>
+
+
+    <div class="container">
+        <h1 class="my-5">Dati per la Spedizione</h1>
+        <!-- Name  -->
+        <div class="form-group pe-1 col-6 mt-3">
+            <label>Nome*</label>
+            <div>
+                <input class="form-control" type="text" name="customer_name" id="customer_name" v-model="customer_name"/>
+                <span class="error-text">{{ formErrors.customer_name }}</span>
+            </div>
+        </div>
+
+        <!-- Last name  -->
+        <div class="form-group col-6 ps-1 my-4">
+            <label>Cognome*</label>
+            <div>
+                <input class="form-control" type="text" name="customer_lastname" id="customer_lastname" v-model="customer_lastname"/>
+                <span class="error-text">{{ formErrors.customer_lastname }}</span>
+            </div>
+        </div>
+
+        <!-- Delivery Address  -->
+        <div class="form-group col-6 ps-1 my-4">
+            <label>Indirizzo spedizione*</label>
+            <div>
+                <input class="form-control" type="text" name="customer_address" id="customer_address" v-model="customer_address"/>
+                <span class="error-text">{{ formErrors.customer_address }}</span>
+            </div>
+        </div>
+
+        <!-- Email  -->
+        <div class="form-group col-6 ps-1 my-4">
+            <label>Email*</label>
+            <div>
+                <input class="form-control" type="text" name="customer_email" id="customer_email" v-model="customer_email"/>
+                <span class="error-text">{{ formErrors.customer_email }}</span>
+            </div>
+        </div>
+
+        <!-- Phone number -->
+        <div class="form-group col-6 ps-1 my-4">
+            <label>Recapito telefonico*</label>
+            <div>
+                <input class="form-control" type="text" name="customer_phone" id="customer_phone" v-model="customer_phone"/>
+                <span class="error-text">{{ formErrors.customer_phone }}</span>
+            </div>
+        </div>
+    </div>
+
+    
+    
 </template>
 
 <style lang="scss" scoped>
-
+    .error-text {
+    color: red;
+    font-size: 12px;
+    margin-top: 5px;
+}
 </style>
