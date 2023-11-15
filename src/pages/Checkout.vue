bce655ea35e85e5c62224dfe0846cbcbb7a9456c
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-11-15 17:05:13 +0100

    checkout-loader

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -34,7 +34,9 @@ export default {
                 customer_address: "",
                 customer_phone: "",
                 amount_paid: "",
-            }
+            },
+
+            loader: false
         }
     },
 
@@ -119,6 +121,11 @@ export default {
             });
         },
 
+        onClickPayment(){
+            this.loader = true;
+            this.submitPayment();
+        },
+
         submitPayment() {
             if (this.validateForm() && this.dropinInstance) {
                 const self = this;
@@ -130,11 +137,11 @@ export default {
                         .then(response => {
                             console.log(response)
                             self.showUI = !self.showUI;
+                            self.loader = false;
                             self.$router.push({ name: 'success' });
                         })
                         .catch(error => {
                             console.log('Error');
-                            self.$router.push({ name: 'rejected' });
                             // Puoi gestire l'errore qui
                         });
                 });
@@ -263,12 +270,19 @@ export default {
 
                     <!-- Braintree Form -->
                     <div class="mt-3" v-show="showUI">
-                        <h1>Inserisci coordinate di pagamento</h1>
+                        <h4>Inserisci coordinate di pagamento</h4>
                         <div id="dropin-container"></div>
-                        <button type="submit" @click="submitPayment()" class="btn btn-primary">Conferma pagamento</button>
+                        <button type="submit" @click="onClickPayment()" class="btn my-button mt-3">Conferma pagamento</button>
                     </div>
 
                 </form>
+
+                <!-- Loader -->
+
+                <div class="loader d-flex flex-column align-items-center justify-content-center" v-if="loader === true">
+                    <img src="/public/img/loader.gif" alt="loader" class="loader-gif">
+                    <div>Stiamo processando il pagamento...</div>
+                </div>
 
             </div>
 
@@ -277,17 +291,17 @@ export default {
             <div class="col-12 col-md-6">
 
                 <div class="recap-container my-5">
-                    <h5>Riepilogo ordine:</h5>
+                    <p>Riepilogo ordine:</p>
                     <h3>{{ restaurant_name }}</h3>
-                    <div class="border p-3 mt-4">
-                        <div class="single-plate d-flex gap-4" v-for="plate in cartItems">
-                            <p class="w-50">{{ plate.plate_name }}</p>
-                            <p class="w-25">x{{ plate.quantity }}</p>
-                            <p class="w-25">{{ totalPlatePrice(plate.quantity, plate.price) }} €</p>
+                    <div class="border mt-4 p-4">
+                        <div class="single-plate d-flex w-100 justify-content-between" v-for="plate in cartItems">
+                            <p class="w-25">{{ plate.plate_name }}</p>
+                            <p class="">x{{ plate.quantity }}</p>
+                            <p class="">{{ totalPlatePrice(plate.quantity, plate.price) }} €</p>
                         </div>
-                        <div class="d-flex">
-                            <p class="fw-bold w-75">Total Price:</p>
-                            <p class="w-25">{{ dataToSend.amount_paid }} €</p>
+                        <div class="d-flex justify-content-between w-100">
+                            <p class="fw-bold w-25">Total Price:</p>
+                            <p class="">{{ dataToSend.amount_paid }} €</p>
                         </div>
 
                     </div>
@@ -302,4 +316,42 @@ export default {
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    @use "../styles/partials/variables";
+
+    label{
+        padding-bottom: .5rem;
+    }
+    .my-button{
+            background-color: variables.$gold_text;
+            border-radius: 0;
+            padding: .6rem 1.8rem;
+            font-weight: 600;
+            font-size: 1rem;
+            transition-property: background color;
+            transition-duration: 0.7s;
+        
+            &:hover{
+                color: variables.$gold-text;
+                background-color: transparent;
+                border-color: variables.$gold-text;
+            }
+        }
+
+        .loader{
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            width: 100%;
+            height: 100%;
+            transform: translate(-50%, -50%);
+            padding: 20px;
+            background-color: rgba(255, 255, 255, 0.185); 
+            backdrop-filter: blur(20px); 
+            z-index: 999;
+
+            .loader-gif{
+                width: 10%;
+            }
+        }
+</style>
